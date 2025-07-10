3082d1d4b76c1e3d0b18637061c272bb194b2fc0
YiJhouWei <b12501029@ntu.edu.tw>
2025-07-11 00:28:23 +0800

    Update main.c

--- HW3/main3/main.c
+++ HW3/main3/main.c
@@ -14,11 +14,10 @@
 #define end_point_x 4
 #define end_point_y 6
 #define frame_rate 500
-
-int x = start_point_x;
-int y = start_point_y;
 int x_direction = 1;
 int y_direction = 1;
+int x = start_point_x;
+int y = start_point_y;
 int frame_count = 0;
 
 uint8_t framebuffer[8] = {};
@@ -28,19 +27,19 @@ esp_timer_handle_t timer_led;
 void max7219_send(uint8_t address, uint8_t data) {
     uint8_t tx_buf[2] = {address, data};
     spi_transaction_t t = {
-        .length = 16,
         .tx_buffer = tx_buf
+        .length = 16,
     };
-    spi_device_transmit(spi, &t);  // Blocking transmit
+    spi_device_transmit(spi, &t); 
 }
 void max7219_init() {
-    max7219_send(0x0F, 0x00);  // Display test off
-    max7219_send(0x0C, 0x01);  // Shutdown mode off
-    max7219_send(0x0B, 0x07);  // Scan all digits
-    max7219_send(0x0A, 0x07);  // Intensity (0x00 ~ 0x0F)
-    max7219_send(0x09, 0x00);  // No decode mode
+    max7219_send(0x0F, 0x00); 
+    max7219_send(0x0C, 0x01);
+    max7219_send(0x0B, 0x07);
+    max7219_send(0x0A, 0x07);
+    max7219_send(0x09, 0x00); 
     for (int i = 0; i < 8; i++) {
-        max7219_send(i, 0x00); // Clear all rows
+        max7219_send(i, 0x00); 
     }
 }
 
@@ -71,7 +70,6 @@ void on_timer(void* args){
         for(int i = 0 ; i < 8 ; i++)set_led(0,i);
         set_led(x,y);
         printf("%d\n", frame_count);
-
     }
     clear();
     for(int i = 0 ; i < 8 ; i++)set_led(0,i);
@@ -81,10 +79,9 @@ void start_timer() {
     const esp_timer_create_args_t timer_args = {
         .callback = &on_timer,
         .arg = NULL,
-        .dispatch_method = ESP_TIMER_TASK,  // 用任務觸發（預設）
+        .dispatch_method = ESP_TIMER_TASK, 
         .name = "frame_timer"
     };
-    
     esp_timer_create(&timer_args, &timer_led);
     esp_timer_start_periodic(timer_led, frame_rate*1000);
 }
@@ -93,21 +90,18 @@ void app_main(void) {
         .mosi_io_num = PIN_NUM_MOSI,
         .miso_io_num = -1,
         .sclk_io_num = PIN_NUM_CLK,
-        .quadwp_io_num = -1,
         .quadhd_io_num = -1,
         .max_transfer_sz = 2
+        .quadwp_io_num = -1,
     };
-
     spi_device_interface_config_t devcfg = {
-        .clock_speed_hz = 1000000,        // 1 MHz
-        .mode = 0,                         // SPI mode 0
+        .clock_speed_hz = 1000000,
         .spics_io_num = PIN_NUM_CS,
+        .mode = 0, 
         .queue_size = 1
     };
-
     spi_bus_initialize(HSPI_HOST, &buscfg, SPI_DMA_CH_AUTO);
     spi_bus_add_device(HSPI_HOST, &devcfg, &spi);
-
     max7219_init();
     start_timer();
 }
